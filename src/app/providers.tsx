11171abede186e68505b0760f39f4ce0faa907e1
Jersson Stiven Morales Alza <jersson.m@outlook.com>
2023-07-10 10:58:51 -0500

    Add page loading indicator

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,21 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
 
 type Props = {
   children?: React.ReactNode
 }
 
 export const NextAuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>
+  return (
+    <SessionProvider>
+      {children}
+      <ProgressBar
+        height="4px"
+        color="#06b6d4"
+        options={{ showSpinner: false }}
+      />
+    </SessionProvider>
+  )
 }
